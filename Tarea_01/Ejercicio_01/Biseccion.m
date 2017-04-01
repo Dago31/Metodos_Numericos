105c0972bd59e2097698d8812fcab5042684d49c
Dago31 <dagoa7x@gmail.com>
2017-04-01 13:20:39 -0400

    Add files via upload

---
+++ Tarea_01/Ejercicio_01/Biseccion.m
@@ -0,0 +1,15 @@
+function c=Biseccion(fnc,x0,x1,tol)
+tic
+it = 0;
+c = (x0+x1)/2;
+while tol<abs(fnc(c));
+    if fnc(x0)*fnc(c)<0;
+        x1=c
+    else
+        x0=c
+    end
+    c = (x0+x1)/2;
+    it = it +1
+end
+toc
+end
