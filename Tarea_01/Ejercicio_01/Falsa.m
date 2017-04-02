309d9ad3f76ff47dccb74637c48fbcc4469aad2c
Dago31 <dagoa7x@gmail.com>
2017-04-01 22:21:31 -0300

    Update Falsa.m

--- Tarea_01/Ejercicio_01/Falsa.m
+++ Tarea_01/Ejercicio_01/Falsa.m
@@ -1,6 +1,6 @@
 function c = Falsa(fnc, x0, x1, tol)
 tic
-cont = 0
+cont = 0;
 c = (x0*fnc(x1)-x1*fnc(x0))/(fnc(x1)-fnc(x0));
 while tol < abs(fnc(c));
     if fnc(x0)*fnc(c)<0;
@@ -9,7 +9,8 @@ while tol < abs(fnc(c));
         x0 = c;
     end
  c = (x0*fnc(x1)-x1*fnc(x0))/(fnc(x1)-fnc(x0));
- cont = cont + 1
+ cont = cont + 1;
 end
-cont
 toc
+cont
+end
