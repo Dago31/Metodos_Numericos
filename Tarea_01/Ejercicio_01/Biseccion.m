1945aec8893ac424a924d15b53de597639130e29
Dago31 <dagoa7x@gmail.com>
2017-04-01 22:22:15 -0300

    Update Biseccion.m

--- Tarea_01/Ejercicio_01/Biseccion.m
+++ Tarea_01/Ejercicio_01/Biseccion.m
@@ -4,12 +4,13 @@ it = 0;
 c = (x0+x1)/2;
 while tol<abs(fnc(c));
     if fnc(x0)*fnc(c)<0;
-        x1=c
+        x1=c;
     else
-        x0=c
+        x0=c;
     end
     c = (x0+x1)/2;
-    it = it +1
+    it = it +1;
 end
 toc
+it
 end
