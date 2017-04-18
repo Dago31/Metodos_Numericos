185e5d0b90bc23964222089d6da4ad612c077fdc
Dago31 <dagoa7x@gmail.com>
2017-04-18 11:39:07 -0400

    Update cosa.m

--- Tarea_01/cosa.m
+++ Tarea_01/cosa.m
@@ -1,20 +1,26 @@
-function xn=newtonsistema(xo,fnc, tol)
- df=jacobian(fnc);
- cont = 0;
- f=inline(fnc);
- jf=inline(df);
- fxo=f(xo(1),xo(2));
- fpxo=jf(xo(1),xo(2))
- xn=xo-inv(fpxo)*fxo;
- cond = (norm(xn-x0))/(norm(xn));
- while cond >= tol;
-    fxo=f(xo(1),xo(2),xo(3));
+xo=[-3;-2] ;
+ syms x y
+ fname=[x^2+x*y^2-x-2;y^2+x*y^2-y+4];
+ fprima=jacobian(fname);
+ tolerancia=1.e-10;
+maxiter = 30;
+ iter = 1;
+ f=inline(fname);
+ jf=inline(fprima);
+ error=norm(f(xo(1),xo(2)),2);
+ fprintf('error=%12.8f\n', error);
+ while error >= tolerancia
+    fxo=f(xo(1),xo(2));
     fpxo=jf(xo(1),xo(2));
-    xn=xo-inv(fpxo)*fxo;
-    fxn=f(xn(1),xn(2))
-    cond = (norm(xn-x0))/(norm(xn));
-    xo=xn;
-    cont=cont+1;
+    x1=xo-inv(fpxo)*fxo;
+    fx1=f(x1(1),x1(2));
+    error =norm((fx1),2);
+    fprintf(' Iter %2d  raiz x=(%14.9f,%14.9f) f(x)=(%14.9f,%14.9f,\n',iter,x1(1),x1(2),fx1(1),fx1(2));
+    if iter > maxiter
+        fprintf(' Numero maximo de iteraciones excedido \n');
+    return;
+    end
+    xo=x1;
+    iter=iter+1;
  end
- cont
-end
+    
